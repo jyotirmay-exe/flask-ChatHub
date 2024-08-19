2970bd1a3eec786f607b3e4a08b6b654113e8dfa
Jyotirmay <jyotirmay18.jk@gmail.com>
2024-08-19 13:26:47 +0530

    utility module

---
+++ modules/utils.py
@@ -0,0 +1,12 @@
+import random
+import string
+import hashlib
+
+def generate_room_id(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length)).upper()
+
+def hasher(s, algorithm='sha256'):
+    hasher = hashlib.new(algorithm)
+    hasher.update(s.encode())
+    return hasher.hexdigest()
