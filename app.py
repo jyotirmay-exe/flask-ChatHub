19d8862c2c15942712976f2a0589734112ff2faf
Jyotirmay <jyotirmay18.jk@gmail.com>
2024-08-21 12:12:25 +0530

    added logging

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, redirect, url_for
-
 from modules.utils import *
+from modules.logger import *
 
 app = Flask(__name__)
+
+logger = getLogger()
 
 rooms = {}
 
@@ -13,6 +15,7 @@ def newRoom():
         hashed = hasher(room_id)
         if hashed not in rooms and room_id not in rooms.values():
             rooms[hashed] = room_id
+            logger.info(f"Created room: {room_id} ({hashed})")
             break
 
     return f"NEW ROOM HERE - {room_id} - {hashed}"
@@ -21,17 +24,25 @@ def newRoom():
 def joinRoom(room_id):
     hashed = next((key for key in rooms if rooms[key] == room_id), None)
     if not hashed:
+        logger.warning(f"Tried to join missing room: {room_id}")
         return render_template("not_found.html")
-    app.logger.info(f"EXISTING ROOM HERE - {room_id} - {hashed}")
-    return redirect(url_for("chatRoom",room_hash=hashed))
+    
+    logger.info(f"Joined room: {room_id} ({hashed})")
+    return redirect(url_for("chatRoom", room_hash=hashed))
 
 @app.route("/room/<room_hash>")
 def chatRoom(room_hash):
-    app.logger.info(f"This is the existing chat room - {room_hash}")
+    if room_hash not in rooms:
+        logger.warning(f"Tried to access missing room: {room_hash}")
+        return render_template("not_found.html")
+    
+    logger.info(f"Entered chat room: {room_hash} (ID: {rooms[room_hash]})")
+    return render_template("chat_room.html", hashed=room_hash, room_id=rooms[room_hash])
 
 @app.route("/")
 def homepage():
+    logger.info("Homepage accessed")
     return render_template('index.html')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
