67f5a785ba59b2a074bc0c7125dd56bfd779d902
Jyotirmay <jyotirmay18.jk@gmail.com>
2024-08-26 17:31:51 +0530

    update

--- modules/logger.py
+++ modules/logger.py
@@ -7,7 +7,7 @@ def getLogger():
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('%(levelname)s - %(message)s - %(asctime)s', datefmt='%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     console_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
--- modules/utils.py
+++ modules/utils.py
@@ -3,17 +3,31 @@ import string
 import hashlib
 from datetime import datetime
 
-def generate_room_id(length=6):
+def generate_room_id(length=6):  
     characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(length)).upper()
+    room_id = ''.join(random.choice(characters) for _ in range(length)).upper()
+    
+    return room_id
 
-def hasher(s, algorithm='sha256'):
-    hasher = hashlib.new(algorithm)
-    hasher.update(s.encode())
-    return hasher.hexdigest()
+def hasher(s, algorithm='sha256'):    
+    try:
+        hasher = hashlib.new(algorithm)
+        hasher.update(s.encode())
+        hash_value = hasher.hexdigest()
+        return hash_value
+    except ValueError as e:
+        raise
 
-def getDate():
-    return datetime.now().strftime("%d/%m/%y")
+def getDate(date_format="%d/%m/%y"):    
+    try:
+        current_date = datetime.now().strftime(date_format)
+        return current_date
+    except Exception as e:
+        raise
 
-def getTime():
-    return datetime.now().strftime("%H:%M")
+def getTime(time_format="%H:%M"):    
+    try:
+        current_time = datetime.now().strftime(time_format)
+        return current_time
+    except Exception as e:
+        raise
